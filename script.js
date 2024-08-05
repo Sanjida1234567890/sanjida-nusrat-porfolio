33b0ada059862d911e6fc454902f66d5dfc34dca
Sanjida1234567890 <sanjidanusrat70@gmail.com>
2024-08-05 13:31:56 +0000

    we added javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log('hello world')
+
+// declare a variabie and assign it a prompt
+// put the amil inside the mailbox
+// var prompt = prompt('what is your name?');
+
+// display the letter that we save is the mailbox to the Comment
+// console.log(prompt)
+
+// if (10 > 9) {
+//   console. log("yes 10 is greater than 9");
+// } else {
+//   console.log('this is false')
+// }
+
+
+var age = prompt('what is your age?');
+
+if (age < 18 && typeof ahe === "string") {
+  alert('you are a minot');
+} else if (age >= 18 && age<= 60) {
+  alert('you are an aduly!')
+} else {
+ alert('dam you are old!') 
+}
